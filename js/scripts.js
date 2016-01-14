46bd1534be294e1aadc0f930d1d0724c03a9d170
Blake Scott and Christine Tacha <blakerscott@gmail.com>
2016-01-13 16:23:23 -0800

    add some js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-dsgsdfg 
+$(document).ready(function() {
+  var flavors = ['Vanilla', 'Strawberry', 'Chocolate', 'Rocky Road'];
+
+  flavors.forEach(function(flavor) {
+    alert('I really like' + flavor + '!');
+
+});
